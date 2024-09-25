0de892c2ec1dc1c5006f8690a4122e024b6c18e2
aidenwang23 <148901698+aidenwang23@users.noreply.github.com>
2024-09-25 15:47:40 +0000

    Completed

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,34 +1,43 @@
-public class Scoreboard {
+public class Scoreboard 
+{
     private String teamOne;
     private String teamTwo;
     private int teamOneScore;
     private int teamTwoScore;
     private String activeTeam;
 
-    public Scoreboard(String one, String two){
+    public Scoreboard(String one, String two)
+    {
         teamOne = one;
         teamTwo = two;
         activeTeam = teamOne;
     }
 
-    public void recordPlay(int score){
-        if (score > 0 && activeTeam == teamOne){
+    public void recordPlay(int score)
+    {
+        if (score > 0 && activeTeam == teamOne)
+        {
             teamOneScore+=score;
         }
-        if (score > 0 && activeTeam == teamTwo){
+        if (score > 0 && activeTeam == teamTwo)
+        {
             teamTwoScore+=score;
         }
-        if (score == 0){
-            if (activeTeam == teamOne){
+        if (score == 0)
+        {
+            if (activeTeam == teamOne)
+            {
                 activeTeam = teamTwo;
             }
-            else{
+            else
+            {
                 activeTeam = teamOne;
             }
         }
     }
 
-    public String getScore(){
+    public String getScore()
+    {
         return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
     } 
 }
